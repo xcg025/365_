997b64f0559cf71d6df09a656f37af32b4e46c7c
ajmd <xucg021@163.com>
2018-05-13 22:41:34 +0800

    commit

--- bet365/userConfig.py
+++ bet365/userConfig.py
@@ -90,6 +90,7 @@ RULE_HALF = {
                 3:{ 'allow_quick_goal_num':0, 'goal_cancel_forbidden':True, 'one_party_zero':True,
                     'all_goal_times': {
                         1: {'min': 0, 'max': 16},
+                        2: {'min': 15, 'max': 45},
                         3: {'min': 23, 'max': 37}
                     }
                 },
