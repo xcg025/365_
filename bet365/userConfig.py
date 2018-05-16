fe325c700a1d387b16f7666d427c203a67ce5c69
ajmd <xucg021@163.com>
2018-05-16 18:40:14 +0800

    commit

--- bet365/userConfig.py
+++ bet365/userConfig.py
@@ -45,10 +45,10 @@ RULE_FULL = {
             3: {'goal_cancel_forbidden': True, 'latest_goal_times': {'min': 70, 'max': 80}, 'last_half_goals': {'min': 0, 'max': 0},
                 'parties_goals_minus_min': 3,},
 
-            4: { 'goal_cancel_forbidden':True,  'latest_goal_times': {'min': 70, 'max': 82}, 'last_half_goals': {'min': 1, 'max': 1},
+            4: { 'goal_cancel_forbidden':True,  'latest_goal_times': {'min': 70, 'max': 80}, 'last_half_goals': {'min': 1, 'max': 1},
                  'two_goals_interval_min': 5, 'allow_quick_goal_num':0,},
 
-            5: { 'goal_cancel_forbidden':True,  'latest_goal_times': {'min': 60, 'max': 82}, 'last_half_goals': {'min': 1, 'max': 3},
+            5: { 'goal_cancel_forbidden':True,  'latest_goal_times': {'min': 60, 'max': 80}, 'last_half_goals': {'min': 1, 'max': 3},
                  'two_goals_interval_min': 4, 'allow_quick_goal_num':1,
                 },
         },
