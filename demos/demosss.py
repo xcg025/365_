33b84774fe579c2e5225375c71694441292a80a5
ajmd <xucg021@163.com>
2018-05-16 22:49:32 +0800

    commit

--- demos/demosss.py
+++ demos/demosss.py
@@ -1,4 +1,5 @@
-goals_time = [1, 2, 3]
+goals_time = [111, 222, 333]
 # print(goals_time.count())
-for time_index in range(0, len(goals_time)-1):
-    print(time_index)
+# print(range(0, len(goals_time)))
+for time_index in range(0, len(goals_time)):
+    print(goals_time[time_index])
