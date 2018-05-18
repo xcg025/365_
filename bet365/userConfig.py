07154404d88824b48bfe6378c0ad348076891ec9
ajmd <xucg021@163.com>
2018-05-18 18:45:37 +0800

    commit

--- bet365/userConfig.py
+++ bet365/userConfig.py
@@ -50,7 +50,7 @@ RULE_FULL = {
                      3:{
                          'all_goal_times': {
                             1: {'min': 0, 'max': 25},
-                            4: {'min': 45, 'max': 60},
+                            4: {'min': 45, 'max': 65},
                           }
                      }
                  }},
